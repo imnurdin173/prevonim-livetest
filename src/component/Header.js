bf111afa7c6ce655127ed1c5540edcd4c4cd3d51
Imam NRD <31567520+imnurdin173@users.noreply.github.com>
2023-02-28 20:06:25 +0700

    first commit

---
+++ src/component/Header.js
@@ -0,0 +1,13 @@
+function Header() {
+    return (
+        <div>
+            <ul>
+                <li>Home</li>
+                <li>About</li>
+                <li>About2</li>
+            </ul>
+        </div>
+    )
+}
+
+export default Header;
